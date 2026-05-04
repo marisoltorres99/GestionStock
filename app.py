a16f5b0c4af15b4715019872935caf9aaee6e9da
marisoltorres99 <86800286+marisoltorres99@users.noreply.github.com>
2026-05-04 12:41:50 -0300

    mostrat todos los productos

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
+from models.Producto import Producto
+from models.Producto import db
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///productos.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def home():
     return "Proyecto Flask funcionando 🚀"
+
+@app.route("/productos")
+def listar():
+    productos = Producto.query.all()
+    return render_template("productos/index.html", productos=productos)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ models/Producto.py
@@ -0,0 +1,9 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Producto(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    precio = db.Column(db.Float, nullable=False)
+    stock = db.Column(db.Integer, nullable=False)
